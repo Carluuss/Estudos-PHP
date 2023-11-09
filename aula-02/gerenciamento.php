0927e94f5ee3ebfbca0a3ea1b387d258836484eb
Carlos <carlosgemano2003@gmail.com>
2023-11-09 10:26:20 -0300

    Condições

--- aula-02/gerenciamento.php
+++ aula-02/gerenciamento.php
@@ -2,14 +2,14 @@
 $estoque = 100;
 
 $movimentacao = 'saida';
+$quantidade = 10;
 
-if($movimentacao == 'entrada'){
-    $atualizacao = $estoque + 10;
-    
-    echo("O estoque teve uma entrada de $atualizacao");
-}else if($movimentacao == 'saida'){
+if ($movimentacao == 'entrada') {
+    $atualizacao = $estoque + $quantidade;
+
+    echo ("O estoque teve uma entrada de $quantidade <br> O valor do estoque atual é de: $atualizacao");
+} else if ($movimentacao == 'saida') {
     $atualizacao = $estoque - 10;
-    echo("O estoque teve uma saida de $atualizacao");
+    echo ("O estoque teve uma saida de $quantidade <br> O valor do estoque atual é de: $atualizacao");
 }
 ?>
-
