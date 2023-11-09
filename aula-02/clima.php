5624a24ea86a17733ad61246e02ba80c854991cc
Carlos <carlosgemano2003@gmail.com>
2023-11-09 09:47:22 -0300

    Condição

---
+++ aula-02/clima.php
@@ -0,0 +1,9 @@
+<?php
+$dia = 'ensolarado';
+
+if($dia == 'ensolarado'){
+    echo("Juvêncio ira para a praia");
+}else if ($dia == 'chuvoso'){
+    echo("Juvêncio ira para a casa de sua mãe");
+}
+?>
