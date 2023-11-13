44881f747f2254ca43950819903b781f293dbca3
Carlos <carlosgemano2003@gmail.com>
2023-11-13 10:42:18 -0300

    Estrutura de repetição

--- aula-04/tabuadaRepeticao.php
+++ aula-04/tabuadaRepeticao.php
@@ -1,18 +1,19 @@
 <?php
-
 $contador = 0;
-$numero = 6;
+$numero = $_POST['numero'];
 
+/*
 while ($contador <= 10) {
     echo ("<br> $numero x $contador = " . $numero * $contador);
 $contador++;
 
 }
+*/
 
 // usando FOR
-/*
+
 for ($i = 0; $i <= 10; $i++) {
     echo ("<br> $numero x $i = " . $numero * $i);
-}*/
+}
 
 ?>
