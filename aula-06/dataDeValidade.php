34e4a1ff09e01cb5fac7b058824ac9ff5e67638e
Carlos <carlosgemano2003@gmail.com>
2023-11-17 09:04:52 -0300

    Calculando data de validade com for

--- aula-06/dataDeValidade.php
+++ aula-06/dataDeValidade.php
@@ -4,7 +4,15 @@ for ($i = 1; $i <= 10; $i++) {
 
     $ano = rand(2020, 2023);
     $mes = rand(1, 12);
-    $dia = rand(1, 31);
+    if ($mes === 1 || $mes === 3 || $mes === 5 || $mes === 7 || $mes === 8 || $mes === 10 || $mes === 12) {
+        $dia = rand(1, 31);
+
+    } else if ($mes === 2) {
+        $dia = rand(1, 28);
+    } else {
+        $dia = rand(1, 30);
+    }
+
 
     if ($ano === 2023 && $mes >= 7 && $dia >= 1) {
         echo ("Produto$i: Valido<br>Data de Validade: $dia/$mes/$ano<br>");
@@ -16,3 +24,4 @@ for ($i = 1; $i <= 10; $i++) {
     }
 }
 
+?>
