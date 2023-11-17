0250f97a55ff74b93372b37fc6edf77d24859a7e
Carlos <carlosgemano2003@gmail.com>
2023-11-17 09:12:02 -0300

    Calculando média com uso de for

--- aula-07/media.php
+++ aula-07/media.php
@@ -1,16 +1,20 @@
 <?php
-
+$aprovado = $recuperacao = $reprovado = 0;
 for($i = 1; $i <= 15; $i++){
     $n1 = rand(0, 10);
     echo("<br>Aluno$i:<br> média =  $n1<br>");
     if($n1 >= 7){
+        $aprovado++ ;
         echo("Aprovado");
     }else if($n1 < 7 && $n1 >= 4){
+        $recuperacao++;
         echo("Recuperação");
     }else{
+        $reprovado++;
         echo("Reprovado");
     }
 }
+echo("<br> Aprovados: $aprovado<br> Recupeção: $recuperacao<br> Reprovados: $reprovado");
 
 
 ?>
