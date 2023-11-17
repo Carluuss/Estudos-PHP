6eef080ef6574a8ca6cf6d35190cb704e9f054a1
Carlos <carlosgemano2003@gmail.com>
2023-11-17 11:04:30 -0300

    Usando vetores

--- aula-07/media.php
+++ aula-07/media.php
@@ -1,12 +1,13 @@
 <?php
+$nota = [1, 7, 9, 10, 2, 6, 4];
 $aprovado = $recuperacao = $reprovado = 0;
-for ($i = 1; $i <= 15; $i++) {
-    $n1 = rand(0, 10);
-    echo ("<br>Aluno$i:<br> média =  $n1<br>");
-    if ($n1 >= 7) {
+for ($i = 0; $i < 7; $i++) {
+    //$n1 = rand(0, 10);
+    echo ("<br>Aluno$i:<br> média =  $nota[$i]<br>");
+    if ($nota[$i] >= 7) {
         $aprovado++;
         echo ("Aprovado");
-    } else if ($n1 < 7 && $n1 >= 4) {
+    } else if ($nota[$i] < 7 && $nota[$i] >= 4) {
         $recuperacao++;
         echo ("Recuperação");
     } else {
