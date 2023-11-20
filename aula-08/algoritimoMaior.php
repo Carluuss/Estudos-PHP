fd54ec99fbae2d6a228ff4a27c35394dfc5a1a51
Carlos <carlosgemano2003@gmail.com>
2023-11-20 10:56:46 -0300

    Excercicios com uso de arrays

--- aula-08/algoritimoMaior.php
+++ aula-08/algoritimoMaior.php
@@ -2,8 +2,10 @@
 
 $numero = [14, 5, 6, 25, 60, 129, 40, 139, 80];
 
-$posicao = 0;
+$posicao = $numero[0];
 for ($i = 0; $i < count($numero); $i++){
+
+
     $numero[$i] > $posicao ? $posicao = $numero[$i]: null;
 
 }
