248d141e5cc87c716bcf3dd5d0bb1ede5f7c6a0f
Carlos <carlosgemano2003@gmail.com>
2023-11-22 10:52:54 -0300

    Introduzindo banco de dados

--- aula-09/sistemas/produtos.php
+++ aula-09/sistemas/produtos.php
@@ -4,4 +4,19 @@ $produto = $_POST['produto'];
 $valor = $_POST['valor'];
 $quantidade = $_POST['quantidade'];
 $validade = $_POST['validade'];
+
+echo("Produto: $produto<br>Valor: $valor<br> Quantidade: $quantidade<br> Validade: $validade");
+
+//conexão com banco de dados
+
+$conn = new MySQli('localhost', 'root', '', 'produtos_full');
+
+if($conn->connect_error){
+    die("Connection falied: ". $conn->connect_error);
+}else{
+    $sql = "INSERT INTO produto (produto, valor, quantidade, validade)
+    VALUE ('$produto', '$valor', '$quantidade', '$validade' ";
+    mysqli_query($conn, $sql);
+}
+
 ?>
