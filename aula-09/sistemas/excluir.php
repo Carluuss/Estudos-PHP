abc34e129fa2dac1f19f989db3393ea6e702c51b
Carlos <carlosgemano2003@gmail.com>
2023-11-29 09:57:17 -0300

    adicionando função de delete

---
+++ aula-09/sistemas/excluir.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once("conexao.php");
+
+$id = $_POST['id'];
+
+
+$sql = "DELETE FROM produtos  WHERE id = '$id'";
+mysqli_query($conn, $sql);
+
+if(mysqli_affected_rows($conn)){
+    
+    echo '<script>alert("Produto deletado com sucesso")</script>';
+}else{
+    echo '<script>alert("Nenhum produto foi deletado")</script>';
+  
+}
+
+?>
