3059df38587c2dc447d6c18f564d351ee2a0473a
Carlos <carlosgemano2003@gmail.com>
2024-01-22 11:10:49 -0300

    jogo da forca

--- aula11/script.js
+++ aula11/script.js
@@ -16,16 +16,34 @@ function iniciar() {
   cardIniciar.classList.add("d-none");
 }
 
+function limparCampos() {
+  letra = document.getElementById("letra").value = "";
+}
+
+let tentativas = 5;
 function verificar() {
   let chave = document.getElementById("chave").value;
   let letra = document.getElementById("letra").value;
-
+  let mostrarT = document.getElementById("mostrarTentativas");
   for (let i = 0; i < chave.length; i++) {
     if (letra == chave[i]) {
       chaveApoio =
         chaveApoio.substring(0, i) + letra + chaveApoio.substring(i + 1);
     }
   }
+
+  !chave.includes(letra) ? tentativas-- : "";
+
+  if (chaveApoio == chave) {
+    alert("PARABÉNS!!");
+    location.reload();
+  } else if (tentativas == 0) {
+    alert("VOCE PERDEU!");
+    location.reload();
+  }
+
   let palavra = document.getElementById("palavra");
   palavra.innerHTML = chaveApoio;
+  mostrarT.innerHTML = `Tentativas: ${tentativas}`;
+  limparCampos();
 }
