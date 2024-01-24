5ed74dd0685641a2196288af57f5e827d553ef9e
Carlos <carlosgemano2003@gmail.com>
2024-01-24 09:42:59 -0300

    adicionando botao reiniciar

--- aula11/script.js
+++ aula11/script.js
@@ -25,15 +25,18 @@ function verificar() {
   let chave = document.getElementById("chave").value;
   let letra = document.getElementById("letra").value;
   let mostrarT = document.getElementById("mostrarTentativas");
+  let lista = document.getElementById("listaLetras");
+  let itens = document.createElement("li");
+
   for (let i = 0; i < chave.length; i++) {
     if (letra == chave[i]) {
       chaveApoio =
-        chaveApoio.substring(0, i) + letra + chaveApoio.substring(i + 1);
+      chaveApoio.substring(0, i) + letra + chaveApoio.substring(i + 1);
     }
   }
-
+  
   !chave.includes(letra) ? tentativas-- : "";
-
+  
   if (chaveApoio == chave) {
     alert("PARABÉNS!!");
     location.reload();
@@ -41,9 +44,15 @@ function verificar() {
     alert("VOCE PERDEU!");
     location.reload();
   }
-
+  
   let palavra = document.getElementById("palavra");
   palavra.innerHTML = chaveApoio;
   mostrarT.innerHTML = `Tentativas: ${tentativas}`;
+  itens.textContent = letra;
+  lista.appendChild(itens);
   limparCampos();
+}
+
+function reiniciar(){
+  location.reload(true);
 }
