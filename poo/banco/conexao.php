cc8f0e59b347db13efaa456200badd042a25ffd1
Carlos <carlosgemano2003@gmail.com>
2024-02-08 09:30:09 -0300

    criando conexao com metodos

--- poo/banco/conexao.php
+++ poo/banco/conexao.php
@@ -2,17 +2,32 @@
 class Conexao
 {
 
-    public function criarConexao()
+    private function criarConexao()
     {
-        try{//tentará fazer os comandos dentro dessa chave
+        try { //tentará fazer os comandos dentro dessa chave
             $conn = new mysqli('localhost', 'root', '', 'banco');
-        }catch(Exception $e){//caso encontre algum erro dentro do try é o catch é realizado
-        die("Connection failed: ". $conn->connect_error);
-        }finally{
             return $conn;
+        } catch (Exception $e) { //caso encontre algum erro dentro do try é o catch é realizado
+            die("Connection failed: " . $conn->connect_error);
         }
 
     }
+
+    public function inserir($c)
+    {
+        try{
+            $conexao = $this->criarConexao();
+            $sql = "INSERT INTO contas (agencia, conta, saldo, senha) VALUES ('" . $c->getAgencia() . "', '" . $c->getConta() . "', '" . $c->getSaldo() . "', '" . $c->getSenha() . "')";
+            mysqli_query($conexao, $sql);
+        }catch(Exception $e){
+
+            die("Connection failed: " . $e->getMessage());
+
+        }finally{
+            mysqli_close($conexao);
+        }
+    }
+
 }
 
 ?>
--- poo/banco/criarConta.php
+++ poo/banco/criarConta.php
@@ -3,15 +3,7 @@ include("conta.php");
 include("conexao.php");
 $c = new Conta($_POST["agencia"], $_POST["conta"], 0, $_POST["senha"]);
 //conexão
-$conn = new Conexao();
-$conexao = $conn->criarConexao();
-// $conn = new mysqli('localhost', 'root', '', 'banco');
-// if($conn->connect_error){
-//     die("Connection failed: ". $conn->connect_error);
-// }
-
-$sql = "INSERT INTO contas (agencia, conta, saldo, senha) VALUES ('".$c->getAgencia()."', '". $c->getConta()."', '".$c->getSaldo()."', '".$c->getSenha()."')";
-
-mysqli_query($conexao, $sql);
+$conn = new Conexao();//criar objeto da classe conexao
+$conn->inserir($c);
 
 ?>
